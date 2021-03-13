dfad9e057fd45cb22191641f1660958ff6384610
sumeikom <sumeikom@gmail.com>
2021-03-12 20:28:12 -0500

    commit tree 64d1c0386139dcffdf5f028510d1f80cf5e947be

--- lib/school.rb
+++ lib/school.rb
@@ -1,1 +1,27 @@
 # code here!
+
+class School
+
+    attr_accessor :name, :roster
+
+    def initialize(school_name)
+        @school_name =school_name
+        @roster = {}
+    end
+
+    def add_student(name, grade)
+        @roster[grade] ||= []
+        @roster[grade] << name
+    end
+
+    def grade(grade_number)
+        @roster[grade_number]
+    end
+
+    def sort
+        @roster.each do |grade, students|
+            students.sort!
+        end
+    end
+
+end
